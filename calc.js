6df289fdf7ffb63dc66da5e526577bef029e68f6
Dallin Breen <dallinbreen624@gmail.com>
2022-01-18 17:12:56 -0700

    Updated syntax to make more sense

--- calc.js
+++ calc.js
@@ -30,15 +30,17 @@ reader.question("What would you like to calculate?", function(input){
 	} else if (mathSymbol === "^") {
 		console.log(Math.sqrt(num1));
 		console.log(Math.sqrt(num2));
-	} else if (mathSymbol === "**") {
+	} else if (mathSymbol === "sq") {
 		console.log(Math.pow(num1, 2));
 		console.log(Math.pow(num2, 2));
-	} else if (mathSymbol === "***") {
+	} else if (mathSymbol === "cube") {
 		console.log(Math.pow(num1, 3));
 		console.log(Math.pow(num2, 3));
-	} else if (mathSymbol === "****") {
+	} else if (mathSymbol === "raised to") {
 		console.log(Math.pow(num1, num3));
 		console.log(Math.pow(num2, num3));
+	} else if (mathSymbol === "%") {
+		console.log(num1 % num2);
 	} else {
 		console.log("This operaton cannot be done");
 	}
